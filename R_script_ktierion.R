7767bcb2e43b3c6e022c803ad2ae663b35d5d46d
Qi Gao <ygaoqi@gmail.com>
2019-04-11 22:36:19 -0500

    Update R_script_ktierion.R

--- R_script_ktierion.R
+++ R_script_ktierion.R
@@ -2,7 +2,7 @@
 library(magrittr)
 library(dplyr)
 library(readr)
-bat <- read_delim('C:/Users/ygaoq/OneDrive/MyDocuments/2019 Spring/Statistics/Project/networktierionTX.txt', delim = " ", col_names = F)
+bat <- read_delim('C:/Users/ygaoq/OneDrive/MyDocuments/2019 Spring/Statistics/Project/Blockchain-Tokens-Data-Analytics/networktierionTX.txt', delim = " ", col_names = F)
 ##View(bat)
 names(bat) <- c('fromID', 'toID', 'unixTime', 'tokenAmount')
 decimals <- 10^18
@@ -10,5 +10,15 @@ supply <- 1.5 * 10^9
 bat
 
 batFiltered <-filter(bat,tokenAmount < decimals * supply)
-bat_filteredout<- filter(bat,tokenAmount >= decimals * supply)
+dim(batFiltered)
+summarise(batFiltered)
+bat_filteredout<- filter(bat,tokenAmount >= decimals * supply) #why 0 row?
 bat_filteredout
+
+
+#Buyers Data
+buyers<-group_by(batFiltered,fromID)
+dim(buyers)
+buyers
+
+############
