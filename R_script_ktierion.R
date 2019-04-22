7c0b7c108e404997c39b85e4f42ee0a81b32bffb
Qi Gao <ygaoqi@gmail.com>
2019-04-21 22:49:16 -0500

    4-21-2019

--- R_script_ktierion.R
+++ R_script_ktierion.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(fitdistrplus)
+library(DAAG)
 bat <- read_delim('C:/Users/ygaoq/OneDrive/MyDocuments/2019 Spring/Statistics/Project/Blockchain-Tokens-Data-Analytics/networktierionTX.txt', delim = " ", col_names = F)
 ##View(bat)
 names(bat) <- c('fromID', 'toID', 'unixTime', 'tokenAmount')
@@ -32,11 +33,19 @@ dim(buys)
 buys
 buys_sorted_dec<-buys[order(-buys$n),]
 buys_sorted_dec
-#top_10_buyers<-buys_sorted%>%head(10) #sort by decending order and get top 10
-#top_10_buyers
 buys_sorted_asc<-buys[order(buys$n),]
 buys_sorted_asc
 View(buys_sorted_asc)
+
+#top 10 active buyers
+top_10_buyers<-buys_sorted_dec%>%head(10) #sort by decending order and get top 10
+top_10_buyers
+
+top_30_buyers<-buys_sorted_dec%>%head(30) #sort by decending order and get top 30
+
+top_100_buyers<-buys_sorted_dec%>%head(100) #sort by decending order and get top 100
+
+
 buys_less_30<-subset(buys_sorted_asc,n<30)
 View(buys_less_30)
 
@@ -98,8 +107,9 @@ cdfcomp(fit_unif)
 #lines(density(buys_sorted_asc$n))
 ###########################
 
-#####################test##############################
+#####################Plot Distribution curves##############################
 
+#single user
 library("ggplot2")
 all_density <- ggplot(data=buys_less_30) + 
   geom_histogram(bins=30,aes(x = buys_less_30$n, ..density..)) + 
@@ -110,4 +120,66 @@ all_density <- ggplot(data=buys_less_30) +
   stat_function(fun=dexp, args=list(rate=0.5244071),colour="green")
 all_density
 
-###################test################################
+#a pair of users
+batFiltered_pairs<-batFiltered%>% group_by(fromID,toID) %>% summarise(n = n()) %>% ungroup 
+View(batFiltered_pairs)
+
+############################################################################
+
+
+
+##############################Question 2####################################
+tierion_prices <- read_delim("C:/Users/ygaoq/OneDrive/MyDocuments/2019 Spring/Statistics/Project/Blockchain-Tokens-Data-Analytics/tierion", delim = "\t", col_names = T) #load token price data
+names(tierion_prices) <- make.names(names(tierion_prices))
+tierion_prices
+tierion_prices <- tierion_prices %>% mutate(date = as.Date(Date, format = '%m/%d/%Y'))
+tierion_prices
+tierion <- read_delim('C:/Users/ygaoq/OneDrive/MyDocuments/2019 Spring/Statistics/Project/Blockchain-Tokens-Data-Analytics/networktierionTX.txt', delim = " ", col_names = F)
+names(tierion) <- c('fromID', 'toID', 'unixTime', 'tokenAmount')
+decimals <- 10^8
+supply <- 1 * 10^9
+dim(tierion)
+
+tierion_filtered <-filter(tierion,tokenAmount < decimals * supply)
+dim(tierion_filtered)
+View(tierion_filtered)
+str(tierion_filtered)
+
+library(anytime)
+## convert data type of unixTime
+tierion_filtered <- tierion_filtered %>%
+  mutate(date = anydate(unixTime))
+names(tierion_filtered) <- c('fromID', 'toID', 'unixTime', 'tokenAmount', 'date')
+str(tierion_filtered)
+tierion_filtered
+
+## merge the prices and edge
+tierion_merged<-merge(x = tierion_prices, y = tierion_filtered, by = "date", all.x = TRUE)
+View(tierion_merged)
+
+filter_K_tierion_merged<-filter(tierion_merged,toID %in% top_10_buyers$toID)
+View(filter_K_tierion_merged)
+View(top_100_buyers$toID)
+
+#get average price from open and close price and add it as a new column
+filter_K_tierion_merged=transform(filter_K_tierion_merged,average_price= (Open+Close)/2)
+View(filter_K_tierion_merged)
+filered<-filter_K_tierion_merged%>% group_by(toID) %>% summarise(n = n(),average_price=mean(average_price)) %>% ungroup 
+View(filered)
+
+regression<-lm(filered$average_price~filered$n)
+regression
+par(mfcol=c(2,2))
+plot(regression)
+summary(regression)  #explaination of detail result: https://blog.csdn.net/lilanfeng1991/article/details/29627405\
+par(mfcol=c(1,1))
+plot(filered$n,filered$average_price)
+abline(regression)
+############################################################################
+
+
+
+
+
+
+
